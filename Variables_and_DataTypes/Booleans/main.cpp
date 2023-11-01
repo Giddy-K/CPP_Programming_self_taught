37d8e84888f1a3764e58ae9a8a5a9b88917415cd
Giddy-K <gideonkipamet@gmail.com>
2023-11-01 18:22:23 +0300

    additions on boolean file

--- Variables_and_DataTypes/Booleans/main.cpp
+++ Variables_and_DataTypes/Booleans/main.cpp
@@ -2,7 +2,7 @@
 
 int main()
 {
-    bool red_light{false};
+    bool red_light{true};
     bool green_light { false };
 
     if (red_light == true)
@@ -13,5 +13,29 @@ int main()
     {
         std::cout << "Go through!" << std::endl;
     }
+
+    if(green_light)
+    {
+        std::cout << "The light is green!" << std::endl;
+    }
+    else
+    {
+        std::cout << "The light is NOT green." << std::endl;
+    }
+
+    //sizeof()
+    std::cout << "Sizeof(bool) : " << sizeof(bool) << std::endl;
+
+    //Printing out a bool
+    //1 -->> true
+    //0 -->> false
+    std::cout << std::endl;
+    std::cout << "red_light : " << red_light << std::endl;
+    std::cout << "green_light : " << green_light << std::endl;
+
+    std::cout << std::boolalpha;
+    std::cout << "red_light : " << red_light << std::endl;
+    std::cout << "green_light : " << green_light << std::endl;
+
     return 0;
 }
